adfc446520350df2357a0aab1f462959216201b3
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-11-15 10:09:56 -0300

    atividade S1-10

---
+++ my.py
@@ -0,0 +1,58 @@
+# Function 1
+
+list_1 = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+# Function 2
+
+print(list_1[-1])
+
+
+# Function 3
+
+list_1[1] = "kenzie"
+print(list_1)
+
+
+# Function 4
+
+print(list_1[2:5])
+
+
+# Function 5
+
+list_1.append("academy")
+print(list_1)
+
+
+# Function 6
+
+list_1.remove("kenzie")
+list_1.remove("academy")
+print(list_1)
+
+
+# Function 7
+
+list_2 = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6]
+
+
+# Function 8
+
+print(len(list_1))
+print(len(list_2))
+
+
+# Function 9
+
+
+print(list_1.pop())
+print(list_2.pop())
+
+
+# Function 10
+
+
+print(list_1.clear())
+print(list_2.clear())
+
